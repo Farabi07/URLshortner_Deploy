1538c9c9461dfc2bf6c9b8a81d9ca41a55108faf
farhad <farhadkabir1212@gmail.com>
2022-09-10 15:25:06 -0700

    Profile Change

--- URLshortner/settings.py
+++ URLshortner/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    'rest_framework.authtoken',
     'knox',
     'users',
     'authentication',
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=os.path.join(BASE_DIR,"static")
 STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
